9dd9f8c36862381522d2d55a380a504386f0d35e
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-05-21 09:29:45 -0500

    test creating a table in database

---
+++ test/03-creating-table.js
@@ -0,0 +1,38 @@
+const InlineDB = require('inlinedb');
+const {expect} = require('code');
+const fs = require('fs');
+const {idbName: name} = require('../util/config');
+
+describe('03 - Creating a new table in database', () => {
+
+  const idbName = `${name}-table`;
+  const idbFilename = `./${idbName}/.idb`;
+  const tableName = 'table-name';
+  const idb = new InlineDB(idbName);
+
+  idb.createTable(tableName);
+
+  it('should add the table name to idb', done => {
+
+    const expectedConfig = {
+      idbName,
+      tables: {
+        [tableName]: {
+          lastInsertId: 0
+        }
+      }
+    };
+
+    fs.readFile(idbFilename, (err, config) => {
+
+      expect(err).to.not.exist();
+
+      expect(JSON.parse(config.toString())).to.equal(expectedConfig);
+
+      done();
+
+    });
+
+  });
+
+});
